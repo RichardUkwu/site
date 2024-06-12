a53f5a8218b7d0cb7c1091d9d1f5e5a18d8a8245
RichardUkwu <127992394+RichardUkwu@users.noreply.github.com>
2024-06-12 20:04:19 +0100

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import backgroundImage from './assets/img.jpeg'
 const Header = () => {
   return (
-    <header className="header" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <header className="header" style={{ backgroundImage: `url("assets/img.jpeg")` }}>
       <nav className="nav">
         <div className="menu-icon">
-          <img src="src/assets/menu.png" alt="" />
+          <img src="assets/menu.png" alt="" />
         </div>
         <div className="nav-title">ZACH WILLIAMS</div>
       </nav>
@@ -15,10 +14,10 @@ const Header = () => {
       </div>
       <div className="contact-links">
           <a href="https://wa.me/+18034776172" target="_blank" rel="noopener noreferrer">
-            <img src="src/assets/whatsapp.png" alt="WhatsApp" className="contact-icon" />
+            <img src="assets/whatsapp.png" alt="WhatsApp" className="contact-icon" />
           </a>
           <a href="mailto:Management.zach.hq@gmail.com" target="_blank" rel="noopener noreferrer">
-            <img src="src/assets/gmail.png" alt="Gmail" className="contact-icon" />
+            <img src="assets/gmail.png" alt="Gmail" className="contact-icon" />
           </a>
         </div>
     </header>
